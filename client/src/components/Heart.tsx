b85cc1e793afb3050b00da03860b5234474348ba
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-03-09 23:59:03 +0900

    add blinking hearts

--- client/src/components/Heart.tsx
+++ client/src/components/Heart.tsx
@@ -1,27 +1,56 @@
-export const Heart = ({ isEmpty }: { isEmpty?: boolean }) => {
+export const Heart = ({
+  isEmpty,
+  isBlinking,
+}: {
+  isEmpty?: boolean;
+  isBlinking?: boolean;
+}) => {
   const heartColor = isEmpty ? "#A9A9A9" : "#FF0000";
 
+  const blinkingStyle = isBlinking
+    ? {
+        animation: "blinkingHeart 0.5s linear infinite alternate",
+      }
+    : {};
+
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-      <path
-        d="M50 80
+    <>
+      {isBlinking && (
+        <style>
+          {`
+          @keyframes blinkingHeart {
+            0% { opacity: 1; }
+            100% { opacity: 0.2; }
+          }
+        `}
+        </style>
+      )}
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+        style={blinkingStyle}
+      >
+        <path
+          d="M50 80
        C50 80, 85 55, 85 35
        C85 15, 65 10, 50 30
        C35 10, 15 15, 15 35
        C15 55, 50 80, 50 80Z"
-        fill={heartColor}
-        stroke={isEmpty ? "#87CEEB" : "none"}
-        strokeWidth="4"
-      />
-      <path
-        d="M50 75
+          fill={heartColor}
+          stroke={isEmpty ? "none" : "#87CEEB"}
+          strokeWidth="4"
+        />
+        <path
+          d="M50 75
        C50 75, 80 52, 80 35
        C80 18, 65 15, 50 32
        C35 15, 20 18, 20 35
        C20 52, 50 75, 50 75Z"
-        fill={heartColor}
-        stroke="none"
-      />
-    </svg>
+          fill={heartColor}
+          stroke="none"
+        />
+      </svg>
+    </>
   );
 };
--- client/src/components/Hearts.tsx
+++ client/src/components/Hearts.tsx
@@ -1,19 +1,56 @@
 import { MAX_HP } from "@socket-io-game/common";
 import { Heart } from "./Heart";
+import { useEffect, useState } from "react";
 
 export const Hearts = ({ hp }: { hp: number }) => {
+  const [previousHp, setPreviousHp] = useState(MAX_HP);
+
+  const [isBlinking, setIsBlinking] = useState(false);
+
+  const damage = hp >= 0 ? previousHp - hp : previousHp;
+
+  useEffect(() => {
+    setIsBlinking(true);
+    
+
+    setTimeout(() => {
+      setIsBlinking(false);
+      setPreviousHp(hp)
+    }, 2000);
+  }, [hp]);
+
   return (
     <>
-      {[...Array(hp)].map(() => (
+    {isBlinking || hp > 0 ? <>
+      {[...Array(Math.max(hp, 0))].map(() => (
         <div className="w-8 h-8 max-md:w-4 max-md:h-4">
           <Heart />
         </div>
       ))}
-      {[...Array(MAX_HP - hp)].map(() => (
+      {[...Array(Math.max(damage, 0))].map(() => (
+        <div className="w-8 h-8 max-md:w-4 max-md:h-4">
+          <Heart isBlinking={true}/>
+        </div>
+      ))}
+      {[...Array(Math.max(MAX_HP - hp - damage, 0))].map(() => (
+        <div className="w-8 h-8 max-md:w-4 max-md:h-4">
+          <Heart isEmpty={true} />
+        </div>
+      ))}
+    </>
+    :<>
+      {[...Array(Math.max(hp, 0))].map(() => (
+        <div className="w-8 h-8 max-md:w-4 max-md:h-4">
+          <Heart/>
+        </div>
+      ))}
+      {[...Array(Math.max(MAX_HP - hp, 0))].map(() => (
         <div className="w-8 h-8 max-md:w-4 max-md:h-4">
           <Heart isEmpty={true}/>
         </div>
       ))}
+      </>
+    }
     </>
   );
 };
