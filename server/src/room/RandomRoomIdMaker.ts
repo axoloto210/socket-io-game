b58d57bca1efbce194d2f9fc6e2f1aad8b684314
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-03-17 01:50:06 +0900

    roomId生成クラスを追加

---
+++ server/src/room/RandomRoomIdMaker.ts
@@ -0,0 +1,21 @@
+export class RandomRoomIdMaker {
+  private roomIdIndex;
+
+  constructor() {
+    this.roomIdIndex = 0;
+  }
+
+  private getRoomId() {
+    return `random-${Math.floor(this.roomIdIndex / 2)}`;
+  }
+
+  private renewRoomId() {
+    this.roomIdIndex++;
+  }
+
+  fetchRoomId() {
+    const roomId = this.getRoomId();
+    this.renewRoomId();
+    return roomId;
+  }
+}
