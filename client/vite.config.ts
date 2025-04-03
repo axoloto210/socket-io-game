1ed35807795ae1486c9f7e4660c87474f10685f9
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-04-04 07:21:56 +0900

    環境変数からURLを読み込むように

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), Sitemap()],
-  resolve: {
-    alias: {
-      "@socket-io-game/common": resolve(__dirname, "../common/dist"),
+export default defineConfig(({ mode }) => {
+  // https://vite.dev/config/#using-environment-variables-in-config
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      Sitemap({
+        hostname: env.VITE_FRONTEND_URL,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@socket-io-game/common": resolve(__dirname, "../common/dist"),
+      },
     },
-  },
-  optimizeDeps: {
-    include: ["@socket-io-game/common"],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/common/, /node_modules/],
+    optimizeDeps: {
+      include: ["@socket-io-game/common"],
     },
-  },
+    build: {
+      commonjsOptions: {
+        include: [/common/, /node_modules/],
+      },
+    },
+  };
 });
