da3b175e5506d29717ffd0ecbfa3f8f86c7a2097
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-06-01 22:29:35 +0900

    cardGameHandlerの抽象クラスを作成

---
+++ server/src/feature/baseCardGameHandler.ts
@@ -0,0 +1,462 @@
+import { Server, Socket } from "socket.io";
+import {
+  ALL_ITEMS,
+  CARD_GAME_EVENTS,
+  MAX_HP,
+  isRestrictedPair,
+  ROOM_EVENTS,
+  DecidedCardAndItem,
+} from "@socket-io-game/common";
+import {
+  Card,
+  CardGameStatus,
+  Item,
+  PlayerStatus,
+  PlayerStatuses,
+} from "@socket-io-game/common";
+import { Items } from "./items";
+
+type Cards = { card: Card; item?: Item };
+
+type SelectedCardsMap = Map<string, Cards>;
+
+const items = new Items();
+
+interface CardGameHandlerConfig {
+  io: Server;
+  roomId: string;
+}
+
+export abstract class BaseCardGameHandler {
+  protected io: Server;
+  protected roomId: string;
+  protected gameStarted: boolean = false;
+  protected playerMap: Map<string, string> = new Map(); // socket.io -> userName
+  protected cardGameStatus: CardGameStatus = {
+    playerStatuses: {},
+    status: CARD_GAME_EVENTS.PENDING,
+  };
+  protected botId: string = '';
+
+  abstract readonly maxPlayers: number;
+  abstract readonly initialHands: Card[];
+  constructor(config: CardGameHandlerConfig) {
+    const { io, roomId } = config;
+    this.io = io;
+    this.roomId = roomId;
+  }
+
+  canJoin(socket: Socket): boolean {
+    if (this.playerMap.size < this.maxPlayers) {
+      return true;
+    } else {
+      socket.emit(ROOM_EVENTS.ROOM_FULL, {
+        message: "このルームは満員です",
+      });
+      return false;
+    }
+  }
+
+//   abstract setupSocket(socket: Socket, userName: string): boolean;
+  setupSocket(socket: Socket, userName: string): boolean {
+    this.playerMap.set(socket.id, userName);
+
+    socket.on(
+      CARD_GAME_EVENTS.DECIDE_CARD_AND_ITEM,
+      (decidedCardAndItem: DecidedCardAndItem) => {
+        this.handleCardSelect({ socketId: socket.id, decidedCardAndItem: decidedCardAndItem, isBot: false });
+      }
+    );
+
+    // 2人揃ったらゲーム開始
+    if (this.playerMap.size === this.maxPlayers) {
+      this.startGame();
+    }
+
+    return true;
+  }
+
+  private startGame() {
+    this.gameStarted = true;
+    this.io.to(this.roomId).emit(CARD_GAME_EVENTS.START, {
+      players: Array.from(this.playerMap.values()),
+    });
+    // ゲーム開始時の初期化処理
+
+    const newPlayerStatuses: PlayerStatuses = Array.from(
+      this.playerMap.keys()
+    ).reduce((previousValue, currentValue) => {
+      return {
+        ...previousValue,
+        [currentValue]: {
+          userName: this.playerMap.get(currentValue),
+          hp: MAX_HP,
+          hands: structuredClone(this.initialHands),
+          items: structuredClone(items.getInitialItems()),
+        },
+      };
+    }, {});
+    this.cardGameStatus = {
+      status: CARD_GAME_EVENTS.START,
+      playerStatuses: newPlayerStatuses,
+    };
+
+    this.sendGameStatusToClient();
+    this.onGameStart();
+  }
+
+  sendGameStatusToClient() {
+    this.io
+      .to(this.roomId)
+      .emit(CARD_GAME_EVENTS.RECEIVE_CARD_GAME, this.cardGameStatus);
+  }
+
+  cleanupRoom() {
+    this.io.to(this.roomId).emit(ROOM_EVENTS.ROOM_DISMISS, {
+      message: `部屋:${this.roomId}が解散されました。`,
+    });
+    this.playerMap.clear();
+    this.selectedCards.clear();
+    this.gameStarted = false;
+  }
+
+  private selectedCards: SelectedCardsMap = new Map();
+
+  handleCardSelect({
+    socketId,
+    decidedCardAndItem,
+    isBot,
+  }: {
+    socketId: string;
+    decidedCardAndItem: DecidedCardAndItem;
+    isBot: boolean;
+  }) {
+    const playerId = isBot ? this.botId! : socketId;
+
+    // プレイヤーが存在するか確認
+    if (!this.playerMap.has(playerId) || !this.gameStarted) {
+      return;
+    }
+
+    // 既に選択済みの場合は処理しない
+    if (this.selectedCards.has(playerId)) {
+      return;
+    }
+
+    const playerStatus = this.cardGameStatus.playerStatuses[playerId];
+    const selectedCard = playerStatus.hands.find((card) => {
+      return card.cardId === decidedCardAndItem.card.cardId;
+    });
+
+    if (selectedCard === undefined) {
+      console.error("カード未選択によるエラーが発生");
+      return;
+    }
+
+    let selectedItem = playerStatus.items.find((item) => {
+      return item.itemId === decidedCardAndItem.itemId;
+    });
+
+    // 選択したカードとアイテムの組が制約に違反している場合には、アイテムを選択していないこととする。
+    if (
+      isRestrictedPair({
+        power: selectedCard.power,
+        itemId: selectedItem?.itemId,
+      })
+    ) {
+      selectedItem = undefined;
+    }
+
+    // カードの選択を保存
+    this.selectedCards.set(playerId, {
+      card: selectedCard,
+      item: selectedItem,
+    });
+
+    // 選択したカードを手札から削除
+    playerStatus.hands = playerStatus.hands.filter((card) => {
+      return card.cardId !== selectedCard.cardId;
+    });
+    // 選択したアイテムを手札から削除
+    playerStatus.items = playerStatus.items.filter((item) => {
+      return item.itemId !== selectedItem?.itemId;
+    });
+
+    if (this.isAllPlayerSelected()) {
+      this.onGameStart();
+      this.resolveSelectedCards();
+    }
+  }
+
+  // 全プレイヤーがカードを選択したか確認
+  //Bot戦の場合はBotが選択済みかも確認する
+  protected isAllPlayerSelected() {
+    return this.selectedCards.size === this.maxPlayers;
+  }
+
+  protected onGameStart(): void {}
+
+  private resolveSelectedCards() {
+    const players = Array.from(this.selectedCards.entries());
+
+    const selectedCardsInfo = players.reduce<
+      Record<
+        string,
+        {
+          playerName?: string;
+          card?: Card;
+          item?: Item;
+        }
+      >
+    >((acc, [playerId, _]) => {
+      const playerStatus = this.cardGameStatus.playerStatuses[playerId];
+      const selectedCards = this.selectedCards.get(playerId);
+
+      acc[playerId] = {
+        playerName: playerStatus?.userName,
+        card: selectedCards?.card,
+        item: selectedCards?.item,
+      };
+      return acc;
+    }, {});
+
+    // カードゲームのステータスを更新
+    this.cardGameStatus = {
+      ...this.cardGameStatus,
+      status: CARD_GAME_EVENTS.SHOWING_SELECTED_CARDS,
+      selectedCards: selectedCardsInfo,
+    };
+
+    this.sendGameStatusToClient();
+
+    setTimeout(() => {
+      this.resolveRound();
+    }, 5000);
+  }
+
+  private resolveRound() {
+    const players = Array.from(this.selectedCards.entries());
+    const [
+      [player1Key, player1SelectedCards],
+      [player2Key, player2SelectedCards],
+    ] = players;
+
+    if (!player1SelectedCards || !player2SelectedCards) {
+      return;
+    }
+
+    // 勝敗判定
+    this.determineBattleResult(
+      player1Key,
+      player2Key,
+      player1SelectedCards,
+      player2SelectedCards
+    );
+
+    // 次のラウンドの準備
+    this.selectedCards.clear();
+    this.cardGameStatus = {
+      ...this.cardGameStatus,
+      selectedCards: {},
+      status:
+        this.cardGameStatus.status === CARD_GAME_EVENTS.GAME_END
+          ? CARD_GAME_EVENTS.GAME_END
+          : CARD_GAME_EVENTS.RESOLVE,
+    };
+    this.sendGameStatusToClient();
+    this.onRoundEnd();
+  }
+
+  protected onRoundEnd(): void {}
+
+  private determineBattleResult(
+    player1Id: string,
+    player2Id: string,
+    player1Cards: Cards,
+    player2Cards: Cards
+  ) {
+    const player1Status = this.cardGameStatus.playerStatuses[player1Id];
+    const player2Status = this.cardGameStatus.playerStatuses[player2Id];
+
+    const { card: player1SelectedCard, item: player1SelectedItem } =
+      player1Cards;
+    const { card: player2SelectedCard, item: player2SelectedItem } =
+      player2Cards;
+
+    this.applyItemEffects({
+      player1SelectedCard,
+      player2SelectedCard,
+      player1SelectedItem,
+      player2SelectedItem,
+    });
+
+    //カードの勝敗判定とダメージ適用
+    this.applyDamage({
+      player1SelectedCard,
+      player2SelectedCard,
+      player1SelectedItem,
+      player2SelectedItem,
+      player1Status,
+      player2Status,
+    });
+
+    // ゲームの勝敗判定
+    if (player1Status.hp <= 0 || player2Status.hp <= 0) {
+      this.cardGameStatus.status = CARD_GAME_EVENTS.GAME_END;
+    }
+  }
+
+  private applyItemEffects({
+    player1SelectedCard,
+    player2SelectedCard,
+    player1SelectedItem,
+    player2SelectedItem,
+  }: {
+    player1SelectedCard: Card;
+    player2SelectedCard: Card;
+    player1SelectedItem?: Item;
+    player2SelectedItem?: Item;
+  }) {
+    // アイテム処理を行わないケース
+    if (
+      items.isMukouEffect(player1SelectedItem, player2SelectedItem) ||
+      (player1SelectedItem == null && player2SelectedItem == null)
+    ) {
+      return;
+    }
+
+    if (player1SelectedItem?.itemId === ALL_ITEMS.GUSU.itemId) {
+      items.applyGusuEffect(player1SelectedCard, player2SelectedCard);
+    }
+    if (player2SelectedItem?.itemId === ALL_ITEMS.GUSU.itemId) {
+      items.applyGusuEffect(player1SelectedCard, player2SelectedCard);
+    }
+
+    if (player1SelectedItem?.itemId === ALL_ITEMS.RISKY.itemId) {
+      items.applyRiskyEffect(player1SelectedCard);
+    }
+    if (player2SelectedItem?.itemId === ALL_ITEMS.RISKY.itemId) {
+      items.applyRiskyEffect(player2SelectedCard);
+    }
+  }
+
+  private applyDamage({
+    player1SelectedCard,
+    player2SelectedCard,
+    player1SelectedItem,
+    player2SelectedItem,
+    player1Status,
+    player2Status,
+  }: {
+    player1SelectedCard: Card;
+    player2SelectedCard: Card;
+    player1SelectedItem?: Item;
+    player2SelectedItem?: Item;
+    player1Status: PlayerStatus;
+    player2Status: PlayerStatus;
+  }) {
+    //player1 勝利時
+    if (
+      this.isFirstPlayerWin({
+        firstPlayerSelectedCard: player1SelectedCard,
+        firstPlayerSelectedItem: player1SelectedItem,
+        secondPlayerSelectedCard: player2SelectedCard,
+        secondPlayerSelectedItem: player2SelectedItem,
+      })
+    ) {
+      player2Status.hp -= 1;
+      if (items.isMukouEffect(player1SelectedItem, player2SelectedItem)) {
+        return;
+      }
+
+      // アイテム処理
+      // リスキー
+      if (player1SelectedItem?.itemId === ALL_ITEMS.RISKY.itemId) {
+        player2Status.hp -= 1;
+        if (player2Status.hp < 0) {
+          player2Status.hp = 0;
+        }
+      }
+    } // player2 勝利時
+    else if (
+      this.isFirstPlayerWin({
+        firstPlayerSelectedCard: player2SelectedCard,
+        firstPlayerSelectedItem: player2SelectedItem,
+        secondPlayerSelectedCard: player1SelectedCard,
+        secondPlayerSelectedItem: player1SelectedItem,
+      })
+    ) {
+      player1Status.hp -= 1;
+      if (items.isMukouEffect(player1SelectedItem, player2SelectedItem)) {
+        return;
+      }
+
+      // アイテム処理
+      // リスキー
+      if (player2SelectedItem?.itemId === 3) {
+        player1Status.hp -= 1;
+        if (player1Status.hp < 0) {
+          player1Status.hp = 0;
+        }
+      }
+    } // 引き分け時
+    else {
+      let basePoint = 1;
+
+      if (items.isMukouEffect(player1SelectedItem, player2SelectedItem)) {
+        player1Status.hp -= basePoint;
+        player2Status.hp -= basePoint;
+        return;
+      }
+      const { player1BasePoint, player2BasePoint } = items.getDrawBasePoint(
+        basePoint,
+        player1SelectedItem,
+        player2SelectedItem
+      );
+
+      player1Status.hp -= player1BasePoint;
+      player2Status.hp -= player2BasePoint;
+    }
+  }
+
+  /**
+   * 引数に渡したfirstPlayerがラウンドで勝った時には真。
+   * @param param0
+   * @returns
+   */
+  private isFirstPlayerWin({
+    firstPlayerSelectedCard,
+    secondPlayerSelectedCard,
+    firstPlayerSelectedItem,
+    secondPlayerSelectedItem,
+  }: {
+    firstPlayerSelectedCard: Card;
+    secondPlayerSelectedCard: Card;
+    firstPlayerSelectedItem?: Item;
+    secondPlayerSelectedItem?: Item;
+  }): boolean {
+    if (
+      items.isMukouEffect(firstPlayerSelectedItem, secondPlayerSelectedItem)
+    ) {
+      if (firstPlayerSelectedCard.power > secondPlayerSelectedCard.power) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if (items.isAbekobe(firstPlayerSelectedItem, secondPlayerSelectedItem)) {
+      if (firstPlayerSelectedCard.power < secondPlayerSelectedCard.power) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      if (firstPlayerSelectedCard.power > secondPlayerSelectedCard.power) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
