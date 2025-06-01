4de82201d6ff0005dab34f558d5d25d054b887f5
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2025-06-01 23:57:18 +0900

    基本ゲームの手札の初期値を定数化

---
+++ server/src/gameConstants.ts
@@ -0,0 +1,24 @@
+import { Card } from "@socket-io-game/common";
+
+export const BASIC_INITIAL_CARDS: Card[] = [
+  {
+    cardId: 1,
+    power: 1,
+  },
+  {
+    cardId: 2,
+    power: 2,
+  },
+  {
+    cardId: 3,
+    power: 3,
+  },
+  {
+    cardId: 4,
+    power: 4,
+  },
+  {
+    cardId: 5,
+    power: 5,
+  },
+] as const;
